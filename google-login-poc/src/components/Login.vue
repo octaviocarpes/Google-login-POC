a0e91f1dc7d0fb55edbc19c1876a01ad61dc4c9d
octavio.carpes <octavio.carpes10@gmail.com>
2018-10-17 23:02:25 -0300

    updated gitignore

---
+++ google-login-poc/src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+ 
+</template>
+
+<script>
+export default {
+  name: "login",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
